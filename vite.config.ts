ef84fe40f81e1bced4cee235ac4f585e3bf74b54
Milan Sampath <ieeerasbmsit@gmail.com>
2025-05-19 22:39:18 +0530

    fixed an error in the package.json file

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base:"/sop-25/",
+  base:"/sop25/",
   server: {
     host: "::",
     port: 8080,
