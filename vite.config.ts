a5c124637971bcabc0ca4de3a4ad8e43713f71a9
Milan Sampath <ieeerasbmsit@gmail.com>
2025-05-19 22:25:25 +0530

    Revert "first commit"
    This reverts commit c36048756d95e9329e211e4fb4c1bea8d4d464ee.

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-import exp from "constants";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/sop-25/',
   server: {
     host: "::",
     port: 8080,
@@ -15,14 +13,10 @@ export default defineConfig(({ mode }) => ({
     react(),
     mode === 'development' &&
     componentTagger(),
-    
   ].filter(Boolean),
-  
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-
 }));
-
