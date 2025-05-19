c36048756d95e9329e211e4fb4c1bea8d4d464ee
Milan Sampath <ieeerasbmsit@gmail.com>
2025-05-19 22:15:49 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import exp from "constants";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/sop-25/',
   server: {
     host: "::",
     port: 8080,
@@ -13,10 +15,14 @@ export default defineConfig(({ mode }) => ({
     react(),
     mode === 'development' &&
     componentTagger(),
+    
   ].filter(Boolean),
+  
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
 }));
+
